6dd2176236e53d8fe7f7683d48e36c96e02688ab
Jasvinder Singh <jvsinghk@gmail.com>
2021-02-12 23:48:55 +0530

    Init Results Container Copmponent

---
+++ src/components/ResultsContainer/ResultsContainer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ResultsContainer = () => {
+    return (
+        <div>
+            <p>This are results</p>
+        </div>
+    );
+};
+
+export default ResultsContainer;
